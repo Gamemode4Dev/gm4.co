77da7ebd62c77f5c104f02902eee1a1e2c83760b
sparks0 <sparks0@users.noreply.github.com>
2021-07-18 11:09:26 +0200

    fixed 3rd party link security

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <p>
     Copyright &copy; <?php echo(date('Y')); ?> Gamemode 4<br>
-    <a href="https://discord.com/invite/erHDVhPY" target="_blank">Discord</a> | <a href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank">Github</a> | <a href="https://twitter.com/GM4Official" target="_blank">Twitter</a>
+    <a href="https://discord.com/invite/erHDVhPY" target="_blank" rel="noopener">Discord</a> | <a href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank" rel="noopener">Github</a> | <a href="https://twitter.com/GM4Official" target="_blank" rel="noopener">Twitter</a>
   </p>
   <p class="footer-small">
     Gamemode 4 is not an official Minecraft product, and is not approved by or associated with Mojang Studios. "Minecraft" is a trademark of Mojang AB and any usage of the Minecraft brand on this site is used in accordance with Mojang Studios' <a href="https://account.mojang.com/terms?ref=ft#brand" target="_blank">Brand and Asset Guidelines</a>.
--- site/index.php
+++ site/index.php
@@ -50,13 +50,13 @@ created 19 august 2020
   </div>
   <div class="landingLinks">
     <h2>Join our community</h2>
-    <a class="discordLink squircleLink" href="https://discord.gg/0qLGgv7JGfIXf45t" target="_blank">
+    <a class="discordLink squircleLink" href="https://discord.gg/0qLGgv7JGfIXf45t" target="_blank" rel="noopener">
       <img src="/images/logo/discord.svg" alt="Discord Logo">Discord
     </a>
-    <a class="githubLink squircleLink" href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank">
+    <a class="githubLink squircleLink" href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank" rel="noopener">
       <img src="/images/logo/github.svg" alt="GitHub Logo">GitHub
     </a>
-    <a class="patreonLink squircleLink" href="https://www.patreon.com/gamemode4" target="_blank">
+    <a class="patreonLink squircleLink" href="https://www.patreon.com/gamemode4" target="_blank" rel="noopener">
       <img src="/images/logo/patreon.svg" alt="Patreon Logo">Patreon
     </a>
   </div>
