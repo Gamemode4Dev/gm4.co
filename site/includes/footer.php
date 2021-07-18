cfb2ba802b1960fc07a2c7a0084854ce75927366
sparks0 <sparks0@users.noreply.github.com>
2021-07-18 11:19:11 +0200

    improved third party link security

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -4,6 +4,6 @@
     <a href="https://discord.com/invite/erHDVhPY" target="_blank" rel="noopener">Discord</a> | <a href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank" rel="noopener">Github</a> | <a href="https://twitter.com/GM4Official" target="_blank" rel="noopener">Twitter</a>
   </p>
   <p class="footer-small">
-    Gamemode 4 is not an official Minecraft product, and is not approved by or associated with Mojang Studios. "Minecraft" is a trademark of Mojang AB and any usage of the Minecraft brand on this site is used in accordance with Mojang Studios' <a href="https://account.mojang.com/terms?ref=ft#brand" target="_blank">Brand and Asset Guidelines</a>.
+    Gamemode 4 is not an official Minecraft product, and is not approved by or associated with Mojang Studios. "Minecraft" is a trademark of Mojang AB and any usage of the Minecraft brand on this site is used in accordance with Mojang Studios' <a href="https://account.mojang.com/terms?ref=ft#brand" target="_blank" rel="noopener">Brand and Asset Guidelines</a>.
   </p>
 </footer>
