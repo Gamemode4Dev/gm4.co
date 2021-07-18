7a11afd36ee67a42d657e93708190c1ec8851582
sparks0 <sparks0@users.noreply.github.com>
2021-07-18 13:20:38 +0200

    updated ancient google analytics scripts

--- site/index.php
+++ site/index.php
@@ -6,6 +6,16 @@ created 19 august 2020
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63061711-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-63061711-1');
+</script>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="theme-color" content="#4AA0C7">
@@ -23,14 +33,6 @@ created 19 august 2020
 <link href="https://fonts.googleapis.com/css?family=Raleway:200" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="includes/homepage.css?hash=<?php echo hash_file("crc32","includes/homepage.css"); ?>" />
 <title>Gamemode 4</title>
-<script async="" src="//www.google-analytics.com/analytics.js"></script><script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-63061711-1', 'auto');
-  ga('send', 'pageview');
-</script>
 <script src="https://www.gm4.co/includes/jquery-3.6.0.min.js"></script>
 </head>
 <body class="light">
