40ba76ea4f695f74255b3e830e124783ea1154e3
Sparks <sparks0@users.noreply.github.com>
2021-07-25 19:19:42 +0200

    added patreon link to footer

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <p>
     Copyright &copy; <?php echo(date('Y')); ?> Gamemode 4<br>
-    <a href="https://discord.com/invite/erHDVhPY" target="_blank" rel="noopener">Discord</a> | <a href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank" rel="noopener">Github</a> | <a href="https://twitter.com/GM4Official" target="_blank" rel="noopener">Twitter</a>
+    <a href="https://discord.com/invite/erHDVhPY" target="_blank" rel="noopener">Discord</a> | <a href="https://github.com/Gamemode4Dev/GM4_Datapacks" target="_blank" rel="noopener">Github</a> | <a href="https://twitter.com/GM4Official" target="_blank" rel="noopener">Twitter</a> | <a href="https://www.patreon.com/gamemode4" target="_blank" rel="noopener">Patreon</a>
   </p>
   <p class="footer-small">
     Gamemode 4 is not an official Minecraft product, and is not approved by or associated with Mojang Studios. "Minecraft" is a trademark of Mojang AB and any usage of the Minecraft brand on this site is used in accordance with Mojang Studios' <a href="https://account.mojang.com/terms?ref=ft#brand" target="_blank" rel="noopener">Brand and Asset Guidelines</a>.
