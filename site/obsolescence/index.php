ca3512dbc478433790c81149afad935a94f5a206
Sparks <sparks0@users.noreply.github.com>
2021-10-24 17:39:04 +0200

    added page

---
+++ site/obsolescence/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63061711-1"></script>
+<script src="https://gm4.co/includes/lazysizes.min.js" async=""></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-63061711-1');
+</script>
+<link rel="stylesheet" href="obsolescence.css?hash=<?php echo hash_file("crc32","obsolescence.css"); ?>" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#4AA0C7">
+<meta name="keywords" content="CTM, complete the monument, adventure map, data packs, data pack, datapack, datapacks, minecraft, minecraft java, commands, mods, mod, plugins, plugin, vanilla, how to install, community, quality of life, balanced, server, public, free, 1.17, 1.16, 1.15, 1.14, 1.13">
+<meta name="description" content="A complete-the-monument adventure map featuring Gamemode 4">
+<meta property="og:site_name" content="Gamemode 4">
+<meta property="og:title" content="Gamemode 4: Obsolescence"/>
+<meta property="og:description" content="A complete-the-monument adventure map featuring Gamemode 4">
+<meta property="og:image" content="https://gm4.co/images/logo/logo_256.png">
+<meta property="og:url" content="https://gm4.co/obsolescence">
+<meta property="og:image:width" content="256">
+<meta property="og:image:height" content="256">
+
+<link rel="icon" type="image/svg" href="../images/logo/logo_clear.svg" sizes="any">
+<title>Gamemode 4: Obsolescence</title>
+<script src="https://www.gm4.co/includes/jquery-3.6.0.min.js"></script>
+</head>
+<body class="light">
+<?php include '../includes/header.php';?>
+<div id="poster">
+<h1>Gamemode 4: Obsolescence</h1>
+<p>A complete-the-monument adventure map featuring Gamemode 4<p>
+<h2>Coming Soon</h2>
+
+</div>
+
+<?php include '../includes/footer.php';?>
+</body>
