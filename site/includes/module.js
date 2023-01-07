90c887fa4b7f294e1d13a4ee743b6d3a4decaf46
Misode <Misoloo64@gmail.com>
2023-01-07 12:08:54 +0100

    Add fallback to jsdelivr for module fetching

--- site/includes/module.js
+++ site/includes/module.js
@@ -20,7 +20,12 @@ function fetchModulesAndResources() {
 	const flatModuleSources = MODULE_SOURCES.flatMap(({ type, repo, versions }) =>
 		versions.map(version => ({ type, version, repo })));
 	return Promise.all(flatModuleSources.map(async ({ type, repo, version }) => {
-		const meta = await fetch(`https://raw.githubusercontent.com/${repo}/release/${version.id}/meta.json`).then(r => r.json());
+		let meta;
+		try {
+			meta = await fetch(`https://raw.githubusercontent.com/${repo}/release/${version.id}/meta.json`).then(r => r.json());
+		} catch (e) {
+			meta = await fetch(`https://cdn.jsdelivr.net/gh/${repo}@release/${version.id}/meta.json`).then(r => r.json());
+		}
 		return { type, version, ...meta };
 	})).then((sources) => {
 		for (const source of sources) {
