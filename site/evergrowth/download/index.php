bc4c7738196796169f3ffe1d08eadb1fa9d1f13d
ToffeeMax <toffeemaxmc@gmail.com>
2023-12-19 21:32:03 +0000

    Add direct download link

---
+++ site/evergrowth/download/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Evergrowth Download Link</title>
+      <meta http-equiv = "refresh" content = "0; url = https://github.com/Gamemode4Dev/evergrowth/releases/latest/download/Evergrowth.zip" />
+   </head>
+   <body>
+      <p>Evergrowth Download Re-direct to GitHub!</p>
+   </body>
+</html>
