995f65a70545794d86659d2dc27fab0979c4ce56
Misode <misoloo64@gmail.com>
2024-03-13 21:24:49 +0100

    Use json file and add styling

--- site/downloads/index.php
+++ site/downloads/index.php
@@ -18,13 +18,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="theme-color" content="#4AA0C7">
-  <meta name="keywords" content="Puzzle Map, 1.20, Minecraft, Datapacks, Custom Resourcepack">
+  <meta name="keywords" content="Minecraft, Datapacks, Public Server, World Downloads">
   <meta name="description" content="A box sliding puzzle map made by Gamemode 4">
   <meta property="og:site_name" content="Gamemode 4">
-  <meta property="og:title" content="Gamemode 4: Evergrowth" />
-  <meta property="og:description" content="A box sliding, mind twisting puzzle map for Minecraft 1.20">
+  <meta property="og:title" content="Gamemode 4: World Downloads" />
+  <meta property="og:description" content="The final world downloads of our past public servers">
   <meta property="og:image" content="https://gm4.co/images/logo/logo_256.png">
-  <meta property="og:url" content="https://gm4.co/evergrowth">
+  <meta property="og:url" content="https://gm4.co/downloads">
   <meta property="og:image:width" content="256">
   <meta property="og:image:height" content="256">
 
@@ -39,111 +39,29 @@
 
 <body class="light">
   <?php include '../includes/header.php'; ?>
-  <div class="slideshow track">
+  <div class="downloadsSlides slideshow track">
     <div class="trackButton trackButtonLeft"></div>
     <div class="trackButton trackButtonRight"></div>
     <div class="trackContainer">
-      <div class="trackItem bottom-left" style="background-image: url(images/slideshow/server7_1.png);">
+      <div class="trackItem bottom-left" style="background-image: url(/images/slideshow/server7_1.png);">
         <h2>Public Server 7</h2>
       </div>
-      <div class="trackItem bottom-left" style="background-image: url(images/server/ps8_players.png);">
+      <div class="trackItem bottom-left" style="background-image: url(/images/server/ps8_players.png);">
         <h2>Public Server 8</h2>
       </div>
-      <div class="trackItem bottom-left" style="background-image: url(images/server/ps8_banner.png);">
+      <div class="trackItem bottom-left" style="background-image: url(/images/server/ps9_banner.png);">
         <h2>Public Server 9</h2>
       </div>
     </div>
   </div>
-
   <div class="infoContainer">
 		<h2>Public Server World Downloads</h2>
-  
     <p>
-      Below are the final world downloads and some pre-season world downloads for each season from the first to the previous.<br />
-      You can expand each season to see the available files, the later season have both files from before server launch and the final ones.
+      Below are the final world downloads and some pre-season world downloads for each season from the first to the previous.
     </p>
   </div>
-  <div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 1</button>
-      <div class="season">
-        <ul>
-          <li><a target="_blank" href="https://drive.google.com/file/d/1jXCigmdiKDQSxvNC6PTM1nc0CvFsaGfd/view?usp=sharing">Final World Download</a> (1.45 GB) Version: 1.8.9 <a class="mirror" href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_1.zip">[Mirror]</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 2</button>
-      <div class="season">
-        <ul>
-          <li><a target="_blank" href="https://drive.google.com/file/d/14gJc4PBzTeLneCIMbUY4g3YRlZrPJjHk/view?usp=sharing">Final World Download</a> (3.28 GB) Version: 1.8.9 <a class="mirror" href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_2.zip">[Mirror]</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 3</button>
-      <div class="season">
-        <ul>
-          <li><a target="_blank" href="https://drive.google.com/file/d/1njppT2EyrKvC8Ntlzw1rF6MU9hfq5f-G/view?usp=sharing">Final World Download</a> (4.47 GB) Version: 1.10.2 <a class="mirror" href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_3-final.zip">[Mirror]</a></li>
-          <li><a target="_blank" href="https://drive.google.com/file/d/1xCT1smm-hLc1VjpOAEbeKsaAGa-XgCGI/view?usp=sharing">Pre Players World Download</a> (2.72 GB) Version: 1.9.2 <a class="mirror" href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_3-pre_players.zip">[Mirror]</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 4 (GM4D)</button>
-      <div class="season">
-        <ul>
-          <li>Jordan to update</li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 5</button>
-      <div class="season">
-        <ul>
-          <li><a target="_blank" href="https://drive.google.com/file/d/1M7-Vz2o8dL-VCxZW-jjM_prLLHTVzkDF/view?usp=sharing">Final World Download</a> (12.5 GB) Version: 1.14.4 <a class="mirror" href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_5-final.zip">[Mirror]</a></li>
-          <li><a target="_blank" href="https://drive.google.com/file/d/1h9stTfXiGZvcHiSFqm9zOkfSHTR3neMS/view?usp=sharing">Pre Players World Download</a> (1.70 GB) Version: 1.13.2 <a class="mirror" href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_5-pre_players.zip">[Mirror]</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 6</button>
-      <div class="season">
-        <ul>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_6-final.zip">Final World Download</a> (8.07 GB) Version: 1.16.5 </li>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_6-pre_players.zip">Pre Players World Download</a> (6.32 GB) Version: 1.15.2 </li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 7 (Sky)</button>
-      <div class="season">
-        <ul>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_7-final.zip">Final World Download</a> (11.57 GB) Version: 1.17.1 </li>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_7-pre_players.zip">Pre Players World Download</a> (13 GB) Version: 1.16.5 </li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 8</button>
-      <div class="season">
-        <ul>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_8-final.zip">Final World Download</a> (7.72 GB) Version: 1.18.2 </li>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_8-pre_players.zip">Pre Players World Download</a> (5.14 GB) Version: 1.18.1 </li>
-        </ul>
-      </div>
-    </div>
-    <div class="content">
-      <button type="button" class="collapsible">Public Server 9</button>
-      <div class="season">
-        <ul>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_9-final.zip">Final World Download</a> (9.03 GB) Version: 1.19.2 </li>
-          <li><a href="https://files.kruthers.com/worlds/gm4/GM4_Public_Server_9-pre_players.zip">Pre Players World Download</a> (8.08 GB) Version: 1.19.2 </li>
-        </ul>
-      </div>
-    </div>
-
-  </div>
+  <main id="downloads">
+  </main>
   <div class="center">
     <?php include '../includes/footer.php'; ?>
   </div>
